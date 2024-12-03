51471a538f680a8353d901f66631816787b16a00
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2024-12-03 17:09:05 +0530

    updated model room, bookde room database

--- src/main/java/org/hotel/villahotel/model/BookedRoom.java
+++ src/main/java/org/hotel/villahotel/model/BookedRoom.java
@@ -1,4 +1,51 @@
 package org.hotel.villahotel.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class BookedRoom {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long bookingId;
+    private LocalDate checkInDate;
+    private LocalDate checkOutDate;
+
+    @Column(name = "guest_FullName")
+    private String guestFullName;
+    private String guestEmail;
+    private int numOfAdults;
+    private int numOfChildren;
+    private int totalNumOfGuests;
+    private String bookingConfirmationCode;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+
+    @JoinColumn(name = "room_id")
+    private Room room;
+
+    public void calculateTotalNumOfGuests() {
+        this.totalNumOfGuests = this.numOfAdults + this.numOfChildren;
+    }
+
+    public void setNumOfChildren(int numOfChildren) {
+        this.numOfChildren = numOfChildren;
+        calculateTotalNumOfGuests();
+    }
+
+    public void setNumOfAdults(int numOfAdults) {
+        this.numOfAdults = numOfAdults;
+        calculateTotalNumOfGuests();
+    }
+
+    public void setBookingConfirmationCode(String bookingConfirmationCode) {
+        this.bookingConfirmationCode = bookingConfirmationCode;
+    }
 }
--- src/main/java/org/hotel/villahotel/model/Room.java
+++ src/main/java/org/hotel/villahotel/model/Room.java
@@ -1,17 +1,45 @@
 package org.hotel.villahotel.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.apache.commons.lang3.RandomStringUtils;
+
 import java.math.BigDecimal;
+import java.sql.Blob;
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
+@Data
+@AllArgsConstructor
 public class Room {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String roomType;
     private BigDecimal roomPrice;
     private boolean isBooked = false;
+
+    @Lob
+    private Blob image;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     private List<BookedRoom> bookings;
 
     public Room() {
         this.bookings = new ArrayList<>();
+    }
+
+    public void addBookedRoom(BookedRoom bookedRoom) {
+        if (bookings == null) {
+            bookings = new ArrayList<>();
+        }
+        bookings.add(bookedRoom);
+        bookedRoom.setRoom(this);
+        isBooked = true;
+        String bookingCode = RandomStringUtils.randomNumeric(10);
+        bookedRoom.setBookingConfirmationCode(bookingCode);
     }
 }
