00a4b628f50761fbd4e612020d9f7a034e346c48
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2024-12-03 21:46:07 +0530

    create addnewRoom content in RoomController

--- src/main/java/org/hotel/villahotel/controller/RoomController.java
+++ src/main/java/org/hotel/villahotel/controller/RoomController.java
@@ -1,5 +1,27 @@
 package org.hotel.villahotel.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.hotel.villahotel.model.Room;
+import org.hotel.villahotel.response.RoomResponse;
+import org.hotel.villahotel.service.IRoomService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.math.BigDecimal;
+
+@RequiredArgsConstructor
 public class RoomController {
+    private final IRoomService roomService;
+
+    public ResponseEntity<RoomResponse> addNewRoom (
+            @RequestParam("photo") MultipartFile photo,
+            @RequestParam("roomType")String roomType,
+            @RequestParam("roomPrice")BigDecimal roomPrice){
+
+        Room savedRoom = roomService.addNewRoom(photo , roomType , roomPrice);
+        RoomResponse response = new RoomResponse(savedRoom.getId(),savedRoom.getRoomType(),savedRoom.getRoomPrice());
+        return ResponseEntity.ok(response);
+    }
 
 }
--- src/main/java/org/hotel/villahotel/service/IRoomService.java
+++ src/main/java/org/hotel/villahotel/service/IRoomService.java
@@ -1,4 +1,10 @@
 package org.hotel.villahotel.service;
 
+import org.hotel.villahotel.model.Room;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.math.BigDecimal;
+
 public interface IRoomService {
+    Room addNewRoom(MultipartFile image, String roomType , BigDecimal roomPrice);
 }
