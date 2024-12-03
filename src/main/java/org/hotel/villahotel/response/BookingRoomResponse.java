728878d295415932c7e3f68bfa5bf1cfd9b10c11
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2024-12-03 18:33:14 +0530

    updated BookingRoomResponse content

--- src/main/java/org/hotel/villahotel/response/BookingRoomResponse.java
+++ src/main/java/org/hotel/villahotel/response/BookingRoomResponse.java
@@ -1,4 +1,29 @@
 package org.hotel.villahotel.response;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hotel.villahotel.model.Room;
+import java.time.LocalDate;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class BookingRoomResponse {
+    private  Long bookingId;
+    private LocalDate checkInDate;
+    private LocalDate checkOutDate;
+    private String guestFullName;
+    private String guestEmail;
+    private int numOfAdults;
+    private int numOfChildren;
+    private int totalNumOfGuests;
+    private String bookingConfirmationCode;
+    private Room room;
+
+    public BookingRoomResponse(Long bookingId, LocalDate checkInDate, LocalDate checkOutDate, String bookingConfirmationCode) {
+        this.bookingId = bookingId;
+        this.checkInDate = checkInDate;
+        this.checkOutDate = checkOutDate;
+        this.bookingConfirmationCode = bookingConfirmationCode;
+    }
 }
