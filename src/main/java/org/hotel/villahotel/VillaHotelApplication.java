d5c2e713746d36d021fef6786866323ecd9f7502
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2024-12-03 09:35:55 +0530

    start villa hotel management system backend

---
+++ src/main/java/org/hotel/villahotel/VillaHotelApplication.java
@@ -0,0 +1,13 @@
+package org.hotel.villahotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VillaHotelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VillaHotelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/hotel/villahotel/VillaHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hotel.villahotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VillaHotelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
